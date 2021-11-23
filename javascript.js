5749857bd0ca0657decb20ab93f5d202964b6257
Tina Eriksson <65234428+TinaEriksson@users.noreply.github.com>
2021-11-23 14:55:56 +0100

    added javascript

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// let buttons = document.querySelectorAll(".btn").length
+
+// for(let i = 0; i < buttons; i++){
+//     document.querySelectorAll()
+// }
+
+function change () {
+    var buttons = document.getElementsByClassName(".btn")
+    buttons.classlist.toggle("changeColor")
+}
