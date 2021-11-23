82e0db28af324eab40fc1c5f4a9fd67d15bd1b74
Tina Eriksson <65234428+TinaEriksson@users.noreply.github.com>
2021-11-23 15:12:44 +0100

    pqaioejfhvpqaoejgfp

--- javascript.js
+++ javascript.js
@@ -1,10 +1,18 @@
 // let buttons = document.querySelectorAll(".btn").length
-
-// for(let i = 0; i < buttons; i++){
-//     document.querySelectorAll()
+// for (var i = 0; i < buttons ; i++) {
+//     document.querySelectorAll(".btn")[i].addEventListener("click", function() {
+//         document.querySelectorAll(".btn")[i].style.backgroundColor = "red";
+//     });
 // }
 
-function change () {
-    var buttons = document.getElementsByClassName(".btn")
-    buttons.classlist.toggle("changeColor")
-}
+// function change () {
+//     var buttons = document.getElementsByClassName("box")
+//     buttons.classlist.toggle("changeColor")
+//     console.log("ok");
+// }
+const myElements = document.querySelectorAll(".btn")
+myElements.forEach(x => x.setAttribute("onclick", "changeColor(this.id)"))
+
+function changeColor(id){
+    document.getElementById(id).style.backgroundColor="red"
+} 
